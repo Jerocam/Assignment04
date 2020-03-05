04af1587f4485b771f0234d3be35e611ef25ef23
Jerocam86 <ocamposjero@gmail.com>
2020-03-05 14:54:45 -0800

    updated

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,10 @@
-
+//FUNCTION $ FOR GETTING ID VALUE AS SHORT CODE
 let $ = function(id){
   return document.getElementById(id);};
 
+//FUNCTION INIT FOR HANDLING BUTTONS
 window.onload=function init (){
+  //NUMBER BUTTONS
   $("nine").onclick = function () {enter(this.value)};
   $("eight").onclick = function() {enter(this.value)};
   $("seven").onclick = function() {enter(this.value)};
@@ -13,20 +15,24 @@ window.onload=function init (){
   $("two").onclick = function() {enter(this.value)};
   $("one").onclick = function() {enter(this.value)}; 
   $("zero").onclick = function() {enter(this.value)};
-
+  
+  //OPERATOR BUTTONS
   $("add").onclick = function() {enter(this.value)};
   $("subtract").onclick = function() {enter(this.value)};
   $("divide").onclick = function() {enter(this.value)};
   $("multiply").onclick = function() {enter(this.value)};
   $("period").onclick = function() {enter(this.value)};
   
+  //RESULT BUTTON FROM =
   $("equal").onclick = function() {calculate()};
 }
 
+//FUNCTION ENTER FOR GETTING NUMBERS INCLUDE OPERATORS FOR VALUE ON TEXT
 function enter(val){
   $("result").value+=val;
 }
 
+//FUNCTION CALCULCATE FOR CALCULATING WITH EVAL RESULTS FROM TEXT THEN PASS THE FINAL VALUE RESULT
 function calculate(){
   let x = $("result").value
   let finalResult = eval(x);
